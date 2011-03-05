c2abd47aae7ad7d5457ce2abbc795fa8641bc014
Arnaud Leymet <arnaud.leymet@gmail.com>
2011-03-06 00:18:50 +0100

    Initial commit - v0.1.0

---
+++ Makefile
@@ -0,0 +1,44 @@
+V ?= 0
+
+SRC_DIR = src
+BUILD_DIR = build
+DIST_DIR = dist
+ASSETS_DIR = assets
+
+PREFIX = .
+
+JS_ENGINE ?= `which node nodejs`
+COMPILER = ${JS_ENGINE} ${BUILD_DIR}/uglify.js --unsafe
+POST_COMPILER = ${JS_ENGINE} ${BUILD_DIR}/post-compile.js
+
+JRC_ORIGIN = ${SRC_DIR}/jquery.jright.js
+JRC = ${DIST_DIR}/jright/jquery.jright.js
+JRC_MIN = ${DIST_DIR}/jright/jquery.jright.min.js
+
+VERSION = $(shell cat version.txt)
+VER = sed "s/@VERSION/${VERSION}/"
+
+DATE=$(shell git log -1 --pretty=format:%ad)
+
+all: jright min
+	@@echo "=== jright build complete. ==="
+
+jright:
+	@@ \
+  echo "* Compiling stylesheets..."; \
+  compass compile -s compressed --css-dir ${DIST_DIR}/jright; \
+  echo "* Copying jright..."; \
+  cp ${JRC_ORIGIN} ${DIST_DIR}/jright; \
+  echo "* Copying assets files..."; \
+  cp ${ASSETS_DIR}/demo.html ${DIST_DIR}; \
+  cp ${ASSETS_DIR}/jquery.client.js ${DIST_DIR}/jright;
+
+min:
+	@@if test ! -z ${JS_ENGINE}; then \
+		echo "* Minifying jright" ${JRC_MIN}; \
+		${COMPILER} ${JRC} > ${JRC_MIN}.tmp; \
+		${POST_COMPILER} ${JRC_MIN}.tmp > ${JRC_MIN}; \
+		rm -f ${JRC_MIN}.tmp; \
+	else \
+		echo "You must have NodeJS installed in order to minify jright."; \
+	fi
---
+++ src/jquery.jright.js
@@ -0,0 +1,114 @@
+/**
+ * jright v0.1
+ * jQuery plugin that enables using custom contextual menus with a native look&feel
+ * https://github.com/arnaud/jright
+ *
+ * Copyright 2011, Arnaud Leymet
+ * Licensed under the MIT license.
+ */
+(function($) {
+  var obj = $(document);
+  var settings = {
+    mode: 'native', // 'native', 'inherit', 'win.ie', 'mac', 'lin.firefox'...
+    parent: document
+  };
+  var methods = {
+    init: function(options) {
+      if ( options ) { 
+        $.extend(settings, options);
+      }
+      obj = this;
+      // Show the menu when right clicking
+      $(document).bind('contextmenu', function(e) {
+        settings.parent = $(e.target);
+        var x = e.pageX;
+        var y = e.pageY;
+        methods.show(x, y);
+        return false;
+      });
+      
+      // Hide the menu when clicking away
+      $('*').not(obj).click(methods.hide);
+    },
+    // show the menu
+    show: function(x, y) {
+      obj
+        .css('left', x)
+        .css('top', y)
+        .fadeIn(50)
+      ;
+      methods.set_style();
+      methods.place();
+    },
+    // hide the menu
+    hide: function() {
+      obj.fadeOut(150);
+    },
+    // automatically detect the OS and the browser
+    // then determines the appropriate stylesheet
+    detect_style: function() {
+      var style = '';
+      switch($.client.os) {
+      case 'Mac':
+        return 'mac';
+      case 'Windows':
+        style = 'win';
+        break;
+      case 'Linux':
+        style = 'lin';
+        break;
+      }
+      style += $.client.browser.toLowerCase();
+      return style;
+    },
+    // set the context menu style
+    set_style: function() {
+      var style = '';
+      switch(settings.mode) {
+      case 'native':
+        style = methods.detect_style();
+        break;
+      case 'inherit':
+        style = $(settings.parent).attr('class');
+        break;
+      default:
+        style = settings.mode;
+      }
+      var style = style || methods.detect_style();
+      obj.attr('class', style);
+    },
+    // prevent the context menu from going beyond the visible screen
+    place: function() {
+      // TODO
+      var pos = methods._find_position(obj);
+      obj.css('left', pos.x).css('top', pos.y);
+      obj.find('ul').each(function() {
+        //var pos = methods._find_position($(this));
+        //$(this).css('top', pos.y);
+      });
+    },
+    _find_position: function(o) {
+      var o_w = o.width() + 5;
+      var o_h = o.height() + 5;
+      var o_x = o.position().left;
+      var o_y = o.position().top;
+      var max_width = $(window).width();
+      var max_height = $(window).height();
+      var x = Math.min(o_x, max_width - o_w);
+      var y = Math.min(o_y, max_height - o_h);
+      return {x: x, y: y}
+    }
+  };
+  
+  $.fn.jright = function(method) {
+
+    // Method calling logic
+    if(methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' +  method + ' does not exist on jQuery.jright');
+    }
+  };
+})(jQuery);
